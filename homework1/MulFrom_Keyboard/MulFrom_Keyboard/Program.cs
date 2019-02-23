598bacbf7ee714709c1fa2d391e0858d1792b8f9
wanquan <3144759341@qq.com>
2019-02-23 18:40:06 +0800

    finish the first version of homework1

---
+++ homework1/MulFrom_Keyboard/MulFrom_Keyboard/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MulFrom_Keyboard
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a, b;
+			Console.WriteLine("Please input integer a:");
+			a = Convert.ToInt32(Console.ReadLine());      //get a
+			Console.WriteLine("Please input integer b:");
+			b = Console.Read();                           //get b
+
+			Console.WriteLine("Multiply:{0}*{1}={2}", a, b, a * b);//get result
+
+			Console.WriteLine("Enter any word and the process will exit.");
+			System.Console.ReadKey();                              //control exit
+		}
+	}
+}
---
+++ homework1/MulFrom_Txtbox/MulFrom_Txtbox/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MulFrom_Txtbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text == ""||this.textBox2.Text=="")
+            {
+                MessageBox.Show("please value a and b!", "System waring", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+                return;
+            }
+
+            int a  = Convert.ToInt32(this.textBox1.Text.Trim());
+            int b  = Convert.ToInt32(this.textBox2.Text.Trim());
+
+            textBox3.Text = Convert.ToString(a * b);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Console.ReadLine();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Console.ReadLine();
+        }
+
+       
+    }
+}
