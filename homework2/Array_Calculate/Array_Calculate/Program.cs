5ee10e764cfed7374ca9af1decde4dcf46878e75
wanquan <3144759341@qq.com>
2019-03-03 15:39:03 +0800

    send homework2

---
+++ homework2/Array_Calculate/Array_Calculate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Calculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int []num = new int[10];       //定义数组
+            Console.WriteLine("Here are the random array:");
+            Random Random1 = new Random();
+            for(int i=0;i<num.Length;i++)
+            {
+                num[i] = Random1.Next(0, 101);//初始化数组
+                Console.Write(num[i] + "\0");
+                //if (i / 4 == 0) { Console.Write("\n"); }
+            }
+            int min = 100, max = 0, sum = 0;
+            foreach(int j in num)
+            {
+                sum+=j;
+                if (j < min) { min = j; }
+                if (j > max) { max = j; }
+            }
+            Console.WriteLine("\nSum:" + sum);
+            Console.WriteLine("Max:" + max);
+            Console.WriteLine("Min:" + min);
+            Console.WriteLine("Average:" + (Convert.ToDouble(sum)/num.Length));
+        }
+    }
+}
---
+++ homework2/Egypt_Sieve/Egypt_Sieve/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egypt_Sieve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here are prime number from 2 to 100:");
+
+            bool []is_prime = new bool[101];//素数位对应值false，合数位对应值true
+            for(int i=2;i<is_prime.Length;i++)
+            {
+                if(is_prime[i]==false)
+                {
+                    Console.WriteLine(i);  //打印素数
+                    for(int j=i*i;j<=100;j+=i)
+                    { is_prime[j] = true; }//将素数i的倍数去除
+                }
+            }
+
+           
+        }
+    }
+}
